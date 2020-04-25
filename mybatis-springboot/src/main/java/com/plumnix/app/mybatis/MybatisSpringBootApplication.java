119ec87636266bf86c7b5acb52e08ea8e62c15cd
plumnix <pocketlx@hotmail.com>
2020-04-25 21:07:21 +0800

    commit new version

---
+++ mybatis-springboot/src/main/java/com/plumnix/app/mybatis/MybatisSpringBootApplication.java
@@ -0,0 +1,15 @@
+package com.plumnix.app.mybatis;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@MapperScan("com.plumnix.app.mybatis.mapper")
+@SpringBootApplication
+public class MybatisSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisSpringBootApplication.class, args);
+    }
+
+}
